5ce81ae7a679e607d940a4c9eacff47d61fc7e3e
Carvalho <pedrocarvalho@gmail.com>
2023-08-14 15:12:23 -0300

    treis

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,6 +2,12 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [usuario, setUsuario] = useState("")
+
+  function handleUsuario(evento) {
+    setUsuario(evento.target.value)
+  }
+  console.log(usuario)
 
   return (
     <>
@@ -13,11 +19,18 @@ function App() {
       <form action="">
         <div>
           <label htmlFor="usuario">Usuário</label>
-          <input type="text" id='usuario' placeholder='Digite seu usuário'/>
+          <input 
+          type="text" 
+          id='usuario' 
+          placeholder='Digite seu usuário' 
+          onChange={handleUsuario}/>
         </div>
         <div>
           <label htmlFor="senha">Senha</label>
-          <input type="password" id='senha' placeholder='Digite sua senha' />
+          <input 
+          type="password" 
+          id='senha' 
+          placeholder='Digite sua senha' />
         </div>
         <button>Entrar</button>
       </form>
